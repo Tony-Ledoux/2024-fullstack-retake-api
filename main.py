b314cf2d9f559b998f347641a1f37e4d02782506
Tony Ledoux <tony.ledoux@student.thomasmore.be>
2024-07-28 13:46:07 +0200

    added test route

--- main.py
+++ main.py
@@ -8,3 +8,7 @@ app = FastAPI(docs_url=config.documentation_url)
 @app.get("/")
 def root():
     return {"message": "hello world"}
+
+@app.get("/test")
+def test():
+    return {"message": "All is working"}
