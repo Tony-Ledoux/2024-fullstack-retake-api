e08665f323b438bb547213f17a7e75e145e7f195
Tony Ledoux <tony.ledoux@student.thomasmore.be>
2024-07-28 12:15:43 +0200

    init

--- database.py
+++ database.py
@@ -4,7 +4,7 @@ import config
 
 def connect_to_database():
     try:
-        connection = mysql.connector.connect()
+        connection = mysql.connector.connect(host=config.db_host, user=config.db_username, password=config.db_password)
         return connection
     except mysql.connector.Error as err:
         print("Error connecting to database: {}".format(err))
