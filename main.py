c5cde9655bd7fd030e3112f7bd66e3ba9142e300
Tony Ledoux <tony.ledoux@student.thomasmore.be>
2024-08-07 08:03:14 +0200

    middleware error

--- main.py
+++ main.py
@@ -11,7 +11,6 @@ app = FastAPI(docs_url=config.documentation_url)
 origins = config.cors_origins.split(",")
 
 app.add_middleware(CORSMiddleware,
-                   allow_origins=origins,
                    allow_credentials=True,
                    allow_methods=["*"],
                    allow_headers=["*"])
